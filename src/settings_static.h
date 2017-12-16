0a1adad452cce7f1034015eb072cde53ffb4a19b
number Zero <silverunicorn2011@yandex.ru>
2017-12-16 22:03:57 +0300

    Compiles!

--- src/settings_static.h
+++ src/settings_static.h
@@ -2,51 +2,114 @@
 #include <atomic>
 #include <string>
 #include <unordered_map>
+#include "util/string.h"
 
+template <typename Settings>
 struct Setting;
 
-class StaticSettings {
+template <typename Settings>
+class StaticSettingsManager {
 public:
-	typedef std::unordered_map<std::string, Setting *> SettingTypes;
+	typedef std::unordered_map<std::string, Setting<Settings> *> SettingTypes;
+	StaticSettingsManager(SettingTypes &_types, Settings &_settings);
 	std::string get(const std::string &name) const;
 	std::string set(const std::string &name, const std::string &value);
 
-protected:
-	StaticSettings(SettingTypes &types);
-
 private:
-	const SettingTypes &settings;
+	const SettingTypes &types;
+	Settings &settings;
 };
 
+template <typename Settings>
 struct Setting {
-	virtual std::string get(const StaticSettings &) = 0;
-	virtual void set(StaticSettings &, const std::string &) = 0;
+	virtual std::string get(const Settings &) = 0;
+	virtual void set(Settings &, const std::string &) = 0;
 };
 
-struct SettingBool: Setting {
-	std::atomic<bool> StaticSettings:: *const setting;
+template <typename Settings, typename Type>
+struct SettingStd: Setting<Settings> {
+	typedef std::atomic<Type> Settings:: *PSetting;
+	const PSetting setting;
 
-	SettingBool(std::atomic<bool> StaticSettings:: *const _setting);
-	std::string get(const StaticSettings &) override;
-	void set(StaticSettings &, const std::string &) override;
+	SettingStd(PSetting _setting) : setting(_setting)
+	{
+	}
+
+	std::string get(const Settings &settings) override
+	{
+		return std::to_string(settings.*setting);
+	}
 };
 
-struct SettingInt: Setting {
-	std::atomic<int> StaticSettings:: *const setting;
+template <typename Settings>
+struct SettingBool: SettingStd<Settings, bool> {
+	using typename SettingStd<Settings, bool>::PSetting;
+	using SettingStd<Settings, bool>::setting;
+	using SettingStd<Settings, bool>::SettingStd;
+
+	void set(Settings &settings, const std::string &value) override
+	{
+		settings.*setting = is_yes(value);
+	}
+};
+
+template <typename Settings>
+struct SettingInt: SettingStd<Settings, int> {
+	using typename SettingStd<Settings, int>::PSetting;
+	using SettingStd<Settings, int>::setting;
 	const int min;
 	const int max;
 
-	SettingInt(std::atomic<bool> StaticSettings:: *const _setting, int _min = INT_MIN, int _max = INT_MAX);
-	std::string get(const StaticSettings &) override;
-	void set(StaticSettings &, const std::string &) override;
+	SettingInt(PSetting _setting, int _min = INT_MIN, int _max = INT_MAX)
+			: SettingStd<Settings, int>(_setting), min(_min), max(_max)
+	{
+	}
+
+	void set(Settings &settings, const std::string &value) override
+	{
+		settings.*setting = mystoi(value, min, max);
+	}
 };
 
-struct SettingFloat: Setting {
-	std::atomic<float> StaticSettings:: *const setting;
+template <typename Settings>
+struct SettingFloat: SettingStd<Settings, float> {
+	using typename SettingStd<Settings, float>::PSetting;
+	using SettingStd<Settings, float>::setting;
 	const float min;
 	const float max;
 
-	SettingFloat(std::atomic<bool> StaticSettings:: *const _setting, float _min = FLOAt_MIN, float _max = FLOAT_MAX);
-	std::string get(const StaticSettings &) override;
-	void set(StaticSettings &, const std::string &) override;
+	SettingFloat(PSetting _setting, float _min = INT_MIN, float _max = INT_MAX)
+			: SettingStd<Settings, float>(_setting), min(_min), max(_max)
+	{
+	}
+
+	void set(Settings &settings, const std::string &value) override
+	{
+		settings.*setting = mystof(value, min, max);
+	}
 };
+
+template <typename Settings>
+StaticSettingsManager<Settings>::StaticSettingsManager(SettingTypes &_types, Settings &_settings)
+		: types(_types), settings(_settings)
+{
+}
+
+template <typename Settings>
+std::string StaticSettingsManager<Settings>::get(const std::string &name) const
+{
+	auto p = types.find(name);
+	if (p == types.end())
+		return{};
+	return p->second->get(settings);
+}
+
+template <typename Settings>
+std::string StaticSettingsManager<Settings>::set(const std::string &name, const std::string &value)
+{
+	auto p = types.find(name);
+	if (p == types.end())
+		return{};
+	p->second->set(settings, value);
+	return p->second->get(settings);
+}
